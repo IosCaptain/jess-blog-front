8b490263a53dbbb65ec1ea2a42b808b720c638c4
Abel <adavid9988@gmail.com>
2022-01-29 03:29:05 -0800

    fixed article content distribution

--- src/app/article.ts
+++ src/app/article.ts
@@ -1,7 +1,88 @@
 export interface Article {
   id: number;
+  date: string;
   title: string;
-  body: string;
   link: string;
   articleCategory: any;
+
+  par1title: string;
+  par1picture: string; //Used in the img tag under src=""
+  par1body: string;
+
+  par2title: string;
+  par2picture: string;
+  par2body: string;
+
+  par3title: string;
+  par3picture: string;
+  par3body: string;
+
+  par4title: string;
+  par4picture: string;
+  par4body: string;
+
+  par5title: string;
+  par5picture: string;
+  par5body: string;
+
+  par6title: string;
+  par6picture: string;
+  par6body: string;
+
+  par7title: string;
+  par7picture: string;
+  par7body: string;
+
+  par8title: string;
+  par8picture: string;
+  par8body: string;
+
+  par9title: string;
+  par9picture: string;
+  par9body: string;
+
+  par10title: string;
+  par10picture: string;
+  par10body: string;
+
+  par11title: string;
+  par11picture: string;
+  par11body: string;
+
+  par12title: string;
+  par12picture: string;
+  par12body: string;
+
+  par13title: string;
+  par13picture: string;
+  par13body: string;
+
+  par14title: string;
+  par14picture: string;
+  par14body: string;
+
+  par15title: string;
+  par15picture: string;
+  par15body: string;
+
+  par16title: string;
+  par16picture: string;
+  par16body: string;
+
+  par17title: string;
+  par17picture: string;
+  par17body: string;
+
+  par18title: string;
+  par18picture: string;
+  par18body: string;
+
+  par19title: string;
+  par19picture: string;
+  par19body: string;
+
+  par20title: string;
+  par20picture: string;
+  par20body: string;
+
 }
